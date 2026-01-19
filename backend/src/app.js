ae2baac855c9be2669c7bceda02823a83813d7ec
rickwengdev <233955863+rickwengdev@users.noreply.github.com>
2026-01-20 05:02:05 +0800

    fix: Add session middleware configuration in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,17 @@ const app = express();
 
 // Middlewares
 app.use(cors({ origin:'https://saori-483222.web.app', credentials: true }));
+app.use(session({
+  secret: process.env.JWT_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'none',
+    secure: true,      
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000 // 1天
+  }
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('combined', { 
